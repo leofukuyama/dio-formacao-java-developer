700e2ed6c28f209544f03d26e88c889f5bb2b31a
leofukuyama <leofukuyama22@gmail.com>
2025-01-17 16:15:05 -0300

    Estudo de documentação Java

--- Aprendendo a sintaxe Java/java-doc/src/edu/leonardo/primeirodia/Calculadora.java
+++ Aprendendo a sintaxe Java/java-doc/src/edu/leonardo/primeirodia/Calculadora.java
@@ -1,5 +1,24 @@
 package edu.leonardo.primeirodia;
 
+/**
+ * <h1>Calculadora</h1>
+ * A Calculadora realiza operações matemáticas entre números inteiros
+ * <p>
+ * <b>Note:</b> Leia atentamente a documentação desta classes
+ * para desfrutar dos recursos oferecidos pelo autor
+ *
+ * @author  Leonardo Fukuyama
+ * @version 1.0
+ * @since   17/01/2025
+ */
 public class Calculadora {
-
+    /**
+     * Este método é utilizado para somar dois números inteiros
+     * @param numeroUm este é o primeiro parâmetro do método
+     * @param numeroDois este é o segundo parâmetro do método
+     * @return int o resultado deste método é a soma dos dois números.
+     */
+    public int somar(int numeroUm, int numeroDois) {
+        return  numeroUm + numeroDois;
+    }
 }
---
+++ Aprendendo a sintaxe Java/java-doc/src/edu/leonardo/primeirodia/Comentarios.java
@@ -0,0 +1,24 @@
+package edu.leonardo.primeirodia;
+
+public class Comentarios {
+    // Olá, eu sou um comentário em uma única linha
+
+    /* Olá,
+     * Eu sou um comentario
+     * que posso ser mais detalhado
+     * quando necessário
+     */
+
+    /**
+     * Estas duas estrelinhas acima
+     * são para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação.
+     * Que incrível !!!
+     */
+
+
+    public static void main(String[] args) {
+
+    }
+}
