8d8926a7df94b4e271f9ce9b5fd2826bd2d3c019
leofukuyama <leofukuyama22@gmail.com>
2025-01-17 16:42:21 -0300

    Estudo de terminal para execução de código Java

---
+++ Aprendendo a sintaxe Java/java-terminal/src/edu/leonardo/primeirodia/MinhaClasse.java
@@ -0,0 +1,7 @@
+package edu.leonardo.primeirodia;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Olá turma, fui executado pelo Terminal!");
+    }
+}
