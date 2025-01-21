9fce7d9942918aa04a63c14de1e107398ab13e2d
leofukuyama <leofukuyama22@gmail.com>
2025-01-21 17:25:21 -0300

    Estudo de Estruturas Condicionais

---
+++ Escopo e Estruturas Condicionais com Java/controle-fluxo/src/ResultadoEscolar.java
@@ -0,0 +1,17 @@
+public class ResultadoEscolar {
+    public static void main(String[] args) {
+        int nota = 4;
+
+        if (nota >= 7)
+            System.out.println("Aprovado");
+        else if (nota >= 5 && nota < 7)
+            System.out.println("Prova Recuperação");
+        else
+            System.out.println("Reprovado");
+
+        String resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Prova Recuperação" : "Reprovado";
+
+        System.out.println(resultado);
+
+    }
+}
