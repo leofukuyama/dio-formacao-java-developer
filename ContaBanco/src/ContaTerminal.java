d909b6ec700d2f79a04ec00b19f191b5db213a8e
leofukuyama <leofukuyama22@gmail.com>
2025-01-21 16:11:16 -0300

    Configuração de ambiente do projeto

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,8 @@
+public class ContaTerminal {
+    public static void main(String[] args) {
+        // TODO: Conhecer e importar a classe Scanner
+        // TODO: Exibir as mensagens para o nosso usuário
+        // TODO: Obter pela Scanner os valores digitados no terminal
+        // TODO: Exibir a mensagem conta criada
+    }
+}
