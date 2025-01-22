58db89c3ca5ec9f9f9f515c82868ab0eeb364d54
leofukuyama <leofukuyama22@gmail.com>
2025-01-22 18:27:30 -0300

    Estudo de Estruturas de Repetição e Tratamento de Exceções

---
+++ Escopo e Estruturas Condicionais com Java/controle-fluxo/src/AboutMe.java
@@ -0,0 +1,32 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        try {
+            // Criando o objeto scanner
+            Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Digite seu nome: ");
+            String nome = sc.next();
+
+            System.out.println("Digite seu sobrenome: ");
+            String sobrenome = sc.next();
+
+            System.out.println("Digite sua idade: ");
+            int idade = sc.nextInt();
+
+            System.out.println("Digite sua altura: ");
+            double altura = sc.nextDouble();
+
+            // Imprimindo os dados obtidos pelo usuário
+            System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase());
+            System.out.println("Tenho " + idade + " anos");
+            System.out.println("Minha altura é " + altura + "cm");
+            sc.close();
+        } catch (InputMismatchException e) {
+            System.err.println("Os campos idade e altura precisam ser numéricos.");
+        }
+    }
+}
---
+++ Escopo e Estruturas Condicionais com Java/controle-fluxo/src/ExemploExcecao.java
@@ -0,0 +1,16 @@
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class ExemploExcecao {
+    public static void main(String[] args) {
+        // Number valor = Double.valueOf("a1.75");
+        Number valor = null;
+        try {
+            valor = NumberFormat.getInstance().parse("1.57");
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(valor);
+    }
+}
