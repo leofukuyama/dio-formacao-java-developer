50dd591e4c96ec8797d299c9c3ca87dc7aee7125
leofukuyama <leofukuyama22@gmail.com>
2025-01-24 09:30:47 -0300

    Resolução do desafio de controle de fluxo

--- Escopo e Estruturas Condicionais com Java/desafio/src/Application/Contador.java
+++ Escopo e Estruturas Condicionais com Java/desafio/src/Application/Contador.java
@@ -6,24 +6,32 @@ import Exception.ParametrosInvalidosException;
 public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
+
         System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.??;
+        int parametroUm = terminal.nextInt();
         System.out.println("Digite o segundo parâmetro");
-        int parametroDois = terminal.??;
+        int parametroDois = terminal.nextInt();
 
         try {
             //chamando o método contendo a lógica de contagem
             contar(parametroUm, parametroDois);
 
-        }catch (? exception) {
-            //imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+        }catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
         }
 
     }
     static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
         //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
 
         int contagem = parametroDois - parametroUm;
         //realizar o for para imprimir os números com base na variável contagem
+
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
     }
 }
