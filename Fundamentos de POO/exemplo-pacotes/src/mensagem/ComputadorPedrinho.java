71c9b0d92c6fc562f2cddc9a9f9bc17b1f394442
leofukuyama <leofukuyama22@gmail.com>
2025-01-28 15:51:48 -0300

    Estudo dos pilares de POO

--- Fundamentos de POO/exemplo-pacotes/src/mensagem/ComputadorPedrinho.java
+++ Fundamentos de POO/exemplo-pacotes/src/mensagem/ComputadorPedrinho.java
@@ -1,11 +1,44 @@
 package mensagem;
 
+import mensagem.apps.FacebookMessenger;
+import mensagem.apps.MSNMessenger;
+import mensagem.apps.ServicoMensagemInstantanea;
+import mensagem.apps.Telegram;
+
 public class ComputadorPedrinho {
     public static void main(String[] args) {
+        ServicoMensagemInstantanea smi = null;
+        /*
+            NÃO SE SABE QUAL APP
+            MAS QUALQUER UM DEVERÁ ENVIAR E RECEBER MENSAGEM
+        */
+
+        String appEscolhido = "tlg";
+
+        if (appEscolhido.equals("msn")) {
+            smi = new MSNMessenger();
+        } else if (appEscolhido.equals("fbm")) {
+            smi = new FacebookMessenger();
+        } else if (appEscolhido.equals("tlg")) {
+            smi = new Telegram();
+        }
+
+        smi.enviarMensagem();
+        smi.receberMensagem();
+
+        /*System.out.println("MSN");
         MSNMessenger msn = new MSNMessenger();
-
         msn.enviarMensagem();
-
         msn.receberMensagem();
+
+        System.out.println("Facebook");
+        FacebookMessenger fcb = new FacebookMessenger();
+        fcb.enviarMensagem();
+        fcb.receberMensagem();
+
+        System.out.println("Telegram");
+        Telegram tlg = new Telegram();
+        tlg.enviarMensagem();
+        tlg.receberMensagem();*/
     }
 }
