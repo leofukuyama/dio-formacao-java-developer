e160b85d4412b0db6f9979dda8113d1d56d1506c
leofukuyama <leofukuyama22@gmail.com>
2025-01-28 16:48:11 -0300

    Estudo dos pilares de POO

---
+++ Fundamentos de POO/exemplo-pacotes/src/desafio/Sistema.java
@@ -0,0 +1,13 @@
+package desafio;
+
+import desafio.celular.IPhone;
+
+public class Sistema {
+    public static void main(String[] args) {
+        IPhone iPhone = new IPhone();
+
+        iPhone.ligar("(43) 9 9999-9999");
+        iPhone.exibirPagina();
+        iPhone.tocar();
+    }
+}
