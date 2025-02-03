f58c900b86e2c97642ff04ae6b06991ea2dbabdf
leofukuyama <leofukuyama22@gmail.com>
2025-02-03 11:51:40 -0300

    Estudo de Listas Duplamente Encadeadas

---
+++ Estrutura de Dados/projetos/src/com/projeto/listaduplamenteencadeada/ListaDuplamenteEncadeada.java
@@ -0,0 +1,34 @@
+package com.projeto.listaduplamenteencadeada;
+
+public class ListaDuplamenteEncadeada<T> {
+    private NoDuplo<T> primeiroNo;
+    private NoDuplo<T> ultimoNo;
+    private int tamanhoLista;
+
+    public ListaDuplamenteEncadeada() {
+        this.primeiroNo = null;
+        this.ultimoNo = null;
+        this.tamanhoLista = 0;
+    }
+
+    public T get(int index) {
+        return this.getNo(index).getConteudo();
+    }
+
+    private NoDuplo<T> getNo(int index) {
+        NoDuplo<T> noAuxiliar = primeiroNo;
+
+        for (int i = 0; (i < index) && (noAuxiliar != null); i++) {
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+
+        return noAuxiliar;
+    }
+
+    public int size() {
+        return tamanhoLista;
+    }
+
+
+
+}
