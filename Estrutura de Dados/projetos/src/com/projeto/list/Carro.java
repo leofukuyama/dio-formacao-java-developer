64ee554bc6deeb7ec3adb6388f72c186f7972d64
leofukuyama <leofukuyama22@gmail.com>
2025-02-07 10:25:45 -0300

    Estudo de Collections

---
+++ Estrutura de Dados/projetos/src/com/projeto/list/Carro.java
@@ -0,0 +1,40 @@
+package com.projeto.list;
+
+import java.util.Objects;
+
+public class Carro {
+
+    String marca;
+
+    public Carro(String marca) {
+        this.marca = marca;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        com.projeto.list.Carro carro = (com.projeto.list.Carro) o;
+        return Objects.equals(marca, carro.marca);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(marca);
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                "marca='" + marca + '\'' +
+                '}';
+    }
+}
