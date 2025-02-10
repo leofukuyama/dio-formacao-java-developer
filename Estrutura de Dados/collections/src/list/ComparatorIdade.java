3993b036a6a172f0819105eb72240d78d4529f2f
leofukuyama <leofukuyama22@gmail.com>
2025-02-10 09:51:33 -0300

    Estudo de Collections em List

---
+++ Estrutura de Dados/collections/src/list/ComparatorIdade.java
@@ -0,0 +1,10 @@
+package list;
+
+import java.util.Comparator;
+
+public class ComparatorIdade implements Comparator<Gato> {
+    @Override
+    public int compare(Gato gato1, Gato gato2) {
+        return Integer.compare(gato1.getIdade(), gato2.getIdade());
+    }
+}
