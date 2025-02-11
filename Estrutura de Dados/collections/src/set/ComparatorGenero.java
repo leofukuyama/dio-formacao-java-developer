a2060376de6febb386399783b0a27700bfb2f714
leofukuyama <leofukuyama22@gmail.com>
2025-02-11 16:16:27 -0300

    Estudo de Collections em Hash, Set e Tree

---
+++ Estrutura de Dados/collections/src/set/ComparatorGenero.java
@@ -0,0 +1,12 @@
+package set;
+
+import java.util.Comparator;
+
+public class ComparatorGenero implements Comparator<Serie> {
+
+    @Override
+    public int compare(Serie o1, Serie o2) {
+        return o1.getGenero().compareTo(o2.getGenero());
+    }
+
+}
