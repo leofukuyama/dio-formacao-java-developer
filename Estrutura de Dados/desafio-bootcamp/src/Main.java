ea9c08ab7a437f2873d426759f349aeb871df822
leofukuyama <leofukuyama22@gmail.com>
2025-02-17 11:33:10 -0300

    Resolução bootcamp

--- Estrutura de Dados/desafio-bootcamp/src/Main.java
+++ Estrutura de Dados/desafio-bootcamp/src/Main.java
@@ -1,4 +1,6 @@
+import domain.Bootcamp;
 import domain.Curso;
+import domain.Dev;
 import domain.Mentoria;
 
 import java.time.LocalDate;
@@ -15,14 +17,43 @@ public class Main {
         curso2.setDescricao("Descrição Curso JS");
         curso2.setCargaHoraria(4);
 
-        System.out.println(curso1);
-        System.out.println(curso2);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("Mentoria Java");
         mentoria.setDescricao("Descrição Mentoria Java");
         mentoria.setDataLocal(LocalDate.now());
 
-        System.out.println(mentoria);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcampJava = new Bootcamp();
+        bootcampJava.setNome("Bootcamp Java Developer");
+        bootcampJava.setDescricao("Descrição Bootcamp Java Developer");
+        bootcampJava.getConteudos().add(curso1);
+        bootcampJava.getConteudos().add(curso2);
+        bootcampJava.getConteudos().add(mentoria);
+
+        Dev devLeonardo = new Dev();
+        devLeonardo.setNome("Leonardo");
+        devLeonardo.inscreverBootcamp(bootcampJava);
+        System.out.println("Conteúdos Inscritos: " + devLeonardo.getConteudosInscritos());
+        devLeonardo.progredir();
+        devLeonardo.progredir();
+        System.out.println("Conteúdos Concluídos: " + devLeonardo.getConteudosConcluidos());
+        System.out.println("\nConteúdos Inscritos: " + devLeonardo.getConteudosInscritos());
+        System.out.println("XP: " + devLeonardo.calcularTotalXp());
+
+        System.out.println("-------------------------");
+
+        Dev devJoao = new Dev();
+        devJoao.setNome("João");
+        devJoao.inscreverBootcamp(bootcampJava);
+        System.out.println("Conteúdos Inscritos: " + devJoao.getConteudosInscritos());
+        devJoao.progredir();
+        devJoao.progredir();
+        System.out.println("Conteúdos Concluídos: " + devJoao.getConteudosConcluidos());
+        System.out.println("\nConteúdos Inscritos: " + devJoao.getConteudosInscritos());
+        System.out.println("XP: " + devJoao.calcularTotalXp());
     }
 }
--- Estrutura de Dados/desafio-bootcamp/src/domain/Dev.java
+++ Estrutura de Dados/desafio-bootcamp/src/domain/Dev.java
@@ -11,7 +11,7 @@ public class Dev {
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(Bootcamp bootcamp) {
-        this.conteudosConcluidos.addAll(bootcamp.getConteudos());
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
         bootcamp.getDevsInscritos().add(this);
     }
 
