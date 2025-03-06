7510368bcdae7a0451e29161b46ad140daf3fcd7
leofukuyama <leofukuyama22@gmail.com>
2025-03-06 14:52:32 -0300

    feat: Estudo de PrintWriter e Scanner

---
+++ JavaIO/JavaIO/src/Exercicio2IOCaracter.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Exercicio2IOCaracter {
+    public static void lerTecladoEscreverDocumento() throws IOException {
+        PrintWriter pw = new PrintWriter(System.out);
+        pw.println("Digite 3 recomendações de filmes: ");
+        pw.flush();
+
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+
+        File f = new File("recomendacoes.txt");
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter(f.getName()));
+
+        do {
+            bw.write(line);
+            bw.newLine();
+            bw.flush();
+            line = scanner.nextLine();
+        } while (!(line.equalsIgnoreCase("fim")));
+
+        pw.printf("Arquivo \"%s\" foi criado com sucesso!", f.getName());
+
+        pw.close();
+        scanner.close();
+        bw.close();
+    }
+
+    public static void main(String[] args) throws IOException {
+        lerTecladoEscreverDocumento();
+    }
+}
